84d940a73eb760dfc78cb847aa72911c71be552f
Stardock123 <josecorona1460@gmail.com>
2017-05-30 22:27:22 -0600

    Add files via upload

---
+++ ProyectoFinal/Proyecto.X/build/default/production/esclavo 2.p1.d
@@ -0,0 +1,3 @@
+ build/default/production/esclavo\ 2.d  \
+ build/default/production/esclavo\ 2.p1:  \
+ esclavo\ 2.c 
---
+++ ProyectoFinal/Proyecto.X/build/default/production/ultrasonico.p1.d
@@ -0,0 +1,3 @@
+ build/default/production/ultrasonico.d  \
+ build/default/production/ultrasonico.p1:  \
+ ultrasonico.c 
